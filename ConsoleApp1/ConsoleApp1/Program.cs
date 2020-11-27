aba874f0ab33d00c4593ae5f69c8209a99fd0249
unknown <v-lydialiu@microsoft.com>
2020-11-27 03:29:36 +0000

    used for JIT test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl = new Class1();
+
+            cl.SurpressJit(); //set bp here
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
